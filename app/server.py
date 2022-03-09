b06f5a60e005c5c71a787eea8b0bdd9ff1a501c7
Theo <lakshay.khanna93@outlook.com>
2022-03-09 19:17:48 +0530

    static

--- app/server.py
+++ app/server.py
@@ -28,7 +28,7 @@ path = pathlib.Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory=r'static'))
+app.mount('/static', StaticFiles(directory='app/static'))
 
 
 async def download_file(url, dest):
